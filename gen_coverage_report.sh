253c6b9739b625cfd7d492d2c8841d0238553915
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-01-09 06:05:25 +0800

    Update gen_cov script

--- gen_coverage_report.sh
+++ gen_coverage_report.sh
@@ -2,10 +2,32 @@
 path=${PWD##*/}
 project="DinnerDecider"
 covdir="htmlcov"
+include="store/views.py,account/views.py"
 
-cd ${project}
-coverage run --source='.' manage.py test
-coverage report
-coverage html
-cd ${covdir}
+if [ -d ${project} ]; then
+    cd ${project}
+    echo -e "Changing directory to <$project>"
+else
+    echo -e "ERROR: Cannot change directory to <$project>"
+    exit 1
+fi
+if [ -d ${covdir} ]; then
+    rm -r ${covdir}
+    echo -e "Deleting old html file"
+fi
+if [ -f "manage.py" ]; then
+    coverage run --source='.' manage.py test
+    coverage report --include="$include"
+    coverage html --include="$include"
+else
+    echo -e "ERROR: manage.py not exist"
+    exit 1
+fi
+if [ -d ${covdir} ]; then
+    cd ${covdir}
+    echo -e "Changing directory to <$covdir>"
+else
+    echo -e "ERROR: Cannot change directory to <$covdir>"
+    exit 1
+fi
 python -m http.server
